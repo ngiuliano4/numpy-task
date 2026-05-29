5ea139a5224cf23390aac708a1e63959a85da6fd
nellogiuliano <n.giuliano4@studenti.unisa.it>
2026-05-29 11:38:13 +0200

    Ho implementato la funzione richiamando la libreria numpy per poi utilizzare la funzione per il calcolo del rango:

--- task.py
+++ task.py
@@ -11,7 +11,8 @@ def prodotto_scalare(v1: list, v2: list) -> float:
     pass
 
 def rango_matrice(m: list) -> int:
-    """Sub-task 2: Calcola il rango di una matrice."""
+    import numpy as np
+    return int(np.linalg.matrix_rank(np.array(m, dtype=float)))
     pass
 
 def risolvi_sistema_lineare(A: list, b: list) -> np.ndarray:
